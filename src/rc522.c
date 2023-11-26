40e018dddb881d34cf7bea87d51361627e147842
Alija Bobija <bobija.alija@gmail.com>
2023-11-26 16:32:54 +0100

    Extract guards to dedicated file

--- src/rc522.c
+++ src/rc522.c
@@ -5,41 +5,9 @@
 #include <string.h>
 
 #include "rc522.h"
+#include "guards.h"
 
 static const char* TAG = "rc522";
-
-/**
- * @brief Macro guard for *alloc functions.
- *        Wrapping memory allocation expression inside of this macro will ensure that
- *        function, in which this macro is called, is going to immediately return
- *        ESP_ERR_NO_MEM if *alloc function fail to reserve the memory.
- * @param EXP Memory allocation expression
- * @return Exit from caller if EXP returns NULL.
-*/
-#define ALLOC_RET_GUARD(EXP) \
-    if((EXP) == NULL) { return ESP_ERR_NO_MEM; }
-
-#define ESP_ERR_RET_GUARD(EXP) \
-    if((err = (EXP)) != ESP_OK) { return err; }
-
-#define SUCCESS_GUARD_GATE __success_guard_gate
-#define ERROR_GUARD_GATE __error_guard_gate
-#define EXIT_GUARD_GATE __exit_guard_gate
-
-#define JMP_GUARD_GATES(on_error, on_success) \
-    goto SUCCESS_GUARD_GATE; \
-        ERROR_GUARD_GATE: { on_error; goto EXIT_GUARD_GATE; };  \
-        SUCCESS_GUARD_GATE: { on_success; goto EXIT_GUARD_GATE; };  \
-        EXIT_GUARD_GATE: {};
-
-#define ESP_ERR_JMP_GUARD(EXP) \
-    if((err = (EXP)) != ESP_OK) { goto ERROR_GUARD_GATE; }
-
-#define ESP_ERR_LOG_AND_JMP_GUARD(EXP, message) \
-    if((err = (EXP)) != ESP_OK) { ESP_LOGE(TAG, message); goto ERROR_GUARD_GATE; }
-
-#define CONDITION_LOG_AND_JMP_GUARD(EXP, message) \
-    if(EXP) { ESP_LOGE(TAG, message); goto ERROR_GUARD_GATE; }
 
 #define FREE(ptr) \
     if(ptr) { free(ptr); ptr = NULL; }
