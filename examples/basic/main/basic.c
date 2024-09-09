3592357e473a87e43b41d88fff64f41defa54405
Alija Bobija <bobija.alija@gmail.com>
2024-09-09 10:31:04 +0200

    Basic example (#47)
    Added basic example

---
+++ examples/basic/main/basic.c
@@ -0,0 +1,33 @@
+#include <esp_log.h>
+#include <inttypes.h>
+#include "rc522.h"
+
+static const char *TAG = "rc522-demo";
+static rc522_handle_t scanner;
+
+static void rc522_handler(void *arg, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    rc522_event_data_t *data = (rc522_event_data_t *)event_data;
+
+    switch (event_id) {
+        case RC522_EVENT_TAG_SCANNED: {
+            rc522_tag_t *tag = (rc522_tag_t *)data->ptr;
+            ESP_LOGI(TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+        } break;
+    }
+}
+
+void app_main()
+{
+    rc522_config_t config = {
+        .spi.host = VSPI_HOST,
+        .spi.miso_gpio = 25,
+        .spi.mosi_gpio = 23,
+        .spi.sck_gpio = 19,
+        .spi.sda_gpio = 22,
+    };
+
+    rc522_create(&config, &scanner);
+    rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
+    rc522_start(scanner);
+}
