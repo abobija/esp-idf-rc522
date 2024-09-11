e89e9ad0e93a9f9986fde06fdbdf34fe3295560d
Alija Bobija <bobija.alija@gmail.com>
2024-09-11 07:46:54 +0200

    Simplify i2c example

--- examples/i2c/main/i2c.c
+++ examples/i2c/main/i2c.c
@@ -5,36 +5,24 @@
 
 static const char *TAG = "rc522-i2c-example";
 
-#define RC522_I2C_ADDRESS        (0x28)
-#define RC522_I2C_RW_TIMEOUT_MS  (1000)
-#define RC522_I2C_CLOCK_SPEED_HZ (100000)
-#define RC522_I2C_PORT           (I2C_NUM_0)
-#define RC522_I2C_GPIO_SDA       (21)
-#define RC522_I2C_GPIO_SCL       (22)
+#define RC522_I2C_ADDRESS       (0x28)
+#define RC522_I2C_RW_TIMEOUT_MS (1000)
+#define RC522_I2C_PORT          (I2C_NUM_0)
+#define RC522_I2C_GPIO_SDA      (18)
+#define RC522_I2C_GPIO_SCL      (21)
+
+static i2c_config_t i2c_config = {
+    .mode = I2C_MODE_MASTER,
+    .sda_io_num = RC522_I2C_GPIO_SDA,
+    .scl_io_num = RC522_I2C_GPIO_SCL,
+    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+    .scl_pullup_en = GPIO_PULLUP_ENABLE,
+    .master.clk_speed = 100000,
+};
 
 static rc522_handle_t rc522_handle;
 
-static esp_err_t rc522_i2c_init()
-{
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = RC522_I2C_GPIO_SDA,
-        .scl_io_num = RC522_I2C_GPIO_SCL,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = RC522_I2C_CLOCK_SPEED_HZ,
-    };
-
-    esp_err_t err = i2c_param_config(RC522_I2C_PORT, &conf);
-
-    if (err != ESP_OK) {
-        return err;
-    }
-
-    return i2c_driver_install(RC522_I2C_PORT, conf.mode, false, false, 0x00);
-}
-
-static esp_err_t rc522_i2c_send(uint8_t *buffer, uint8_t length)
+static esp_err_t rc522_send(uint8_t *buffer, uint8_t length)
 {
     return i2c_master_write_to_device(RC522_I2C_PORT,
         RC522_I2C_ADDRESS,
@@ -43,7 +31,7 @@ static esp_err_t rc522_i2c_send(uint8_t *buffer, uint8_t length)
         RC522_I2C_RW_TIMEOUT_MS / portTICK_PERIOD_MS);
 }
 
-static esp_err_t rc522_i2c_receive(uint8_t *buffer, uint8_t length, uint8_t address)
+static esp_err_t rc522_receive(uint8_t *buffer, uint8_t length, uint8_t address)
 {
     return i2c_master_write_read_device(RC522_I2C_PORT,
         RC522_I2C_ADDRESS,
@@ -52,16 +40,6 @@ static esp_err_t rc522_i2c_receive(uint8_t *buffer, uint8_t length, uint8_t addr
         buffer,
         length,
         RC522_I2C_RW_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
-
-static esp_err_t rc522_send(uint8_t *buffer, uint8_t length)
-{
-    return rc522_i2c_send(buffer, length);
-}
-
-static esp_err_t rc522_receive(uint8_t *buffer, uint8_t length, uint8_t address)
-{
-    return rc522_i2c_receive(buffer, length, address);
 }
 
 static void rc522_event_handler(void *arg, esp_event_base_t base, int32_t event_id, void *event_data)
@@ -78,7 +56,8 @@ static void rc522_event_handler(void *arg, esp_event_base_t base, int32_t event_
 
 void app_main()
 {
-    rc522_i2c_init();
+    i2c_param_config(RC522_I2C_PORT, &i2c_config);
+    i2c_driver_install(RC522_I2C_PORT, i2c_config.mode, 0, 0, 0x00);
 
     rc522_config_t config = {
         .send_handler = &rc522_send,
