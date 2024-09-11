1db26807aa1f345ee87ae911a25829d6f0aad277
Alija Bobija <bobija.alija@gmail.com>
2024-09-11 07:54:19 +0200

    Move GPIO config to defines

--- examples/basic/main/basic.c
+++ examples/basic/main/basic.c
@@ -5,12 +5,16 @@
 
 static const char *TAG = "rc522-basic-example";
 
-#define RC522_SPI_HOST (VSPI_HOST)
+#define RC522_SPI_HOST            (VSPI_HOST)
+#define RC522_SPI_BUS_GPIO_MISO   (25)
+#define RC522_SPI_BUS_GPIO_MOSI   (23)
+#define RC522_SPI_BUS_GPIO_SCLK   (19)
+#define RC522_SPI_DEVICE_GPIO_SDA (22)
 
 static spi_bus_config_t spi_bus_config = {
-    .miso_io_num = 25,
-    .mosi_io_num = 23,
-    .sclk_io_num = 19,
+    .miso_io_num = RC522_SPI_BUS_GPIO_MISO,
+    .mosi_io_num = RC522_SPI_BUS_GPIO_MOSI,
+    .sclk_io_num = RC522_SPI_BUS_GPIO_SCLK,
     .quadwp_io_num = -1,
     .quadhd_io_num = -1,
 };
@@ -18,7 +22,7 @@ static spi_bus_config_t spi_bus_config = {
 static spi_device_interface_config_t spi_rc522_config = {
     .clock_speed_hz = 5000000,
     .mode = 0,
-    .spics_io_num = 22,
+    .spics_io_num = RC522_SPI_DEVICE_GPIO_SDA,
     .queue_size = 7,
     .flags = SPI_DEVICE_HALFDUPLEX,
 };
