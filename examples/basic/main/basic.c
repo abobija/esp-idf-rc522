0ed3e8f6d2a40ef1baed938d07658194b6f6e48f
Alija Bobija <bobija.alija@gmail.com>
2024-09-15 12:53:56 +0200

    Update examples

--- examples/basic/main/basic.c
+++ examples/basic/main/basic.c
@@ -1,84 +1,47 @@
 #include <esp_log.h>
-#include <driver/spi_master.h>
 #include "rc522.h"
+#include "rc522_driver_spi.h"
 #include "picc/rc522_mifare.h"
 
 static const char *TAG = "rc522-basic-example";
 
-#define RC522_SPI_HOST            (VSPI_HOST)
 #define RC522_SPI_BUS_GPIO_MISO   (25)
 #define RC522_SPI_BUS_GPIO_MOSI   (23)
 #define RC522_SPI_BUS_GPIO_SCLK   (19)
 #define RC522_SPI_DEVICE_GPIO_SDA (22)
 
-static spi_bus_config_t spi_bus_config = {
-    .miso_io_num = RC522_SPI_BUS_GPIO_MISO,
-    .mosi_io_num = RC522_SPI_BUS_GPIO_MOSI,
-    .sclk_io_num = RC522_SPI_BUS_GPIO_SCLK,
-    .quadwp_io_num = -1,
-    .quadhd_io_num = -1,
+static rc522_driver_config_t driver_config = {
+    .spi = {
+        .host_id = VSPI_HOST,
+        .dma_chan = SPI_DMA_DISABLED,
+        .bus_config = &(spi_bus_config_t){
+            .miso_io_num = RC522_SPI_BUS_GPIO_MISO,
+            .mosi_io_num = RC522_SPI_BUS_GPIO_MOSI,
+            .sclk_io_num = RC522_SPI_BUS_GPIO_SCLK,
+            .quadwp_io_num = -1,
+            .quadhd_io_num = -1,
+        },
+        .dev_config = {
+            .clock_speed_hz = 5000000,
+            .mode = 0,
+            .spics_io_num = RC522_SPI_DEVICE_GPIO_SDA,
+            .queue_size = 7,
+            .flags = SPI_DEVICE_HALFDUPLEX,
+        },
+    },
 };
 
-static spi_device_interface_config_t spi_rc522_config = {
-    .clock_speed_hz = 5000000,
-    .mode = 0,
-    .spics_io_num = RC522_SPI_DEVICE_GPIO_SDA,
-    .queue_size = 7,
-    .flags = SPI_DEVICE_HALFDUPLEX,
-};
+static rc522_driver_handle_t driver;
+static rc522_handle_t rc522;
 
-static rc522_handle_t rc522_handle;
-static spi_device_handle_t rc522_spi_handle;
-
-static esp_err_t rc522_send(uint8_t *buffer, uint8_t length)
-{
-    uint8_t *address = (buffer + 0);
-    uint8_t address_origin = *address;
-
-    *address <<= 1;
-    *address &= (~0x80);
-
-    esp_err_t ret = spi_device_polling_transmit(rc522_spi_handle,
-        &(spi_transaction_t) {
-            .length = 8 * length,
-            .tx_buffer = buffer,
-        });
-
-    *address = address_origin;
-
-    return ret;
-}
-
-static esp_err_t rc522_receive(uint8_t address, uint8_t *buffer, uint8_t length)
-{
-    address <<= 1;
-    address |= 0x80;
-
-    spi_device_acquire_bus(rc522_spi_handle, portMAX_DELAY);
-
-    for (uint8_t i = 0; i < length; i++) {
-        spi_device_polling_transmit(rc522_spi_handle,
-            &(spi_transaction_t) {
-                .length = 8,
-                .tx_buffer = &address,
-                .rxlength = 8,
-                .rx_buffer = (buffer + i),
-            });
-    }
-
-    spi_device_release_bus(rc522_spi_handle);
-
-    return ESP_OK;
-}
-
-static esp_err_t rc522_picc_dump(rc522_handle_t rc522, rc522_picc_t *picc)
+static esp_err_t rc522_picc_dump(rc522_handle_t _rc522, rc522_picc_t *picc)
 {
     ESP_LOGI(TAG, "PICC (sak=%02x, type=%s)", picc->sak, rc522_picc_type_name(picc->type));
     ESP_LOGI(TAG, "UID:");
     ESP_LOG_BUFFER_HEX(TAG, picc->uid.value, picc->uid.length);
 
     if (rc522_mifare_type_is_classic_compatible(picc->type)) {
-        return rc522_mifare_dump(rc522,
+        return rc522_mifare_dump(_rc522,
             picc,
             &(rc522_mifare_key_t) {
                 .value = RC522_MIFARE_DEFAULT_KEY_VALUE,
@@ -99,22 +62,21 @@ static void rc522_event_handler(void *arg, esp_event_base_t base, int32_t event_
             rc522_picc_t *picc = (rc522_picc_t *)data->ptr;
 
             ESP_LOGI(TAG, "PICC detected");
-            rc522_picc_dump(rc522_handle, picc);
+            rc522_picc_dump(rc522, picc);
         } break;
     }
 }
 
 void app_main()
 {
-    spi_bus_initialize(RC522_SPI_HOST, &spi_bus_config, 0);
-    spi_bus_add_device(RC522_SPI_HOST, &spi_rc522_config, &rc522_spi_handle);
+    rc522_driver_spi_create(&driver_config, &driver);
+    rc522_driver_install(driver);
 
     rc522_config_t config = {
-        .send_handler = &rc522_send,
-        .receive_handler = &rc522_receive,
+        .driver = driver,
     };
 
-    rc522_create(&config, &rc522_handle);
-    rc522_register_events(rc522_handle, RC522_EVENT_ANY, rc522_event_handler, NULL);
-    rc522_start(rc522_handle);
+    rc522_create(&config, &rc522);
+    rc522_register_events(rc522, RC522_EVENT_ANY, rc522_event_handler, NULL);
+    rc522_start(rc522);
 }
--- examples/i2c/main/i2c.c
+++ examples/i2c/main/i2c.c
@@ -1,55 +1,41 @@
 #include <esp_log.h>
-#include <driver/i2c.h>
 #include "rc522.h"
+#include "rc522_driver_i2c.h"
 #include "picc/rc522_mifare.h"
 
 static const char *TAG = "rc522-i2c-example";
 
-#define RC522_I2C_ADDRESS       (0x28)
-#define RC522_I2C_RW_TIMEOUT_MS (1000)
-#define RC522_I2C_PORT          (I2C_NUM_0)
-#define RC522_I2C_GPIO_SDA      (18)
-#define RC522_I2C_GPIO_SCL      (21)
+#define RC522_I2C_ADDRESS  (0x28)
+#define RC522_I2C_GPIO_SDA (18)
+#define RC522_I2C_GPIO_SCL (21)
 
-static i2c_config_t i2c_config = {
-    .mode = I2C_MODE_MASTER,
-    .sda_io_num = RC522_I2C_GPIO_SDA,
-    .scl_io_num = RC522_I2C_GPIO_SCL,
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .master.clk_speed = 100000,
+static rc522_driver_config_t driver_config = {
+    .i2c = {
+        .port = I2C_NUM_0,
+        .device_address = RC522_I2C_ADDRESS,
+        .rw_timeout_ms = 1000,
+        .config = {
+            .mode = I2C_MODE_MASTER,
+            .sda_io_num = RC522_I2C_GPIO_SDA,
+            .scl_io_num = RC522_I2C_GPIO_SCL,
+            .sda_pullup_en = GPIO_PULLUP_ENABLE,
+            .scl_pullup_en = GPIO_PULLUP_ENABLE,
+            .master.clk_speed = 100000,
+        },
+    },
 };
 
-static rc522_handle_t rc522_handle;
+static rc522_driver_handle_t driver;
+static rc522_handle_t rc522;
 
-static esp_err_t rc522_send(uint8_t *buffer, uint8_t length)
-{
-    return i2c_master_write_to_device(RC522_I2C_PORT,
-        RC522_I2C_ADDRESS,
-        buffer,
-        length,
-        RC522_I2C_RW_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
-
-static esp_err_t rc522_receive(uint8_t address, uint8_t *buffer, uint8_t length)
-{
-    return i2c_master_write_read_device(RC522_I2C_PORT,
-        RC522_I2C_ADDRESS,
-        &address,
-        1,
-        buffer,
-        length,
-        RC522_I2C_RW_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
-
-static esp_err_t rc522_picc_dump(rc522_handle_t rc522, rc522_picc_t *picc)
+static esp_err_t rc522_picc_dump(rc522_handle_t _rc522, rc522_picc_t *picc)
 {
     ESP_LOGI(TAG, "PICC (sak=%02x, type=%s)", picc->sak, rc522_picc_type_name(picc->type));
     ESP_LOGI(TAG, "UID:");
     ESP_LOG_BUFFER_HEX(TAG, picc->uid.value, picc->uid.length);
 
     if (rc522_mifare_type_is_classic_compatible(picc->type)) {
-        return rc522_mifare_dump(rc522,
+        return rc522_mifare_dump(_rc522,
             picc,
             &(rc522_mifare_key_t) {
                 .value = RC522_MIFARE_DEFAULT_KEY_VALUE,
@@ -70,22 +56,21 @@ static void rc522_event_handler(void *arg, esp_event_base_t base, int32_t event_
             rc522_picc_t *picc = (rc522_picc_t *)data->ptr;
 
             ESP_LOGI(TAG, "PICC detected");
-            rc522_picc_dump(rc522_handle, picc);
+            rc522_picc_dump(rc522, picc);
         } break;
     }
 }
 
 void app_main()
 {
-    i2c_param_config(RC522_I2C_PORT, &i2c_config);
-    i2c_driver_install(RC522_I2C_PORT, i2c_config.mode, 0, 0, 0x00);
+    rc522_driver_i2c_create(&driver_config, &driver);
+    rc522_driver_install(driver);
 
     rc522_config_t config = {
-        .send_handler = &rc522_send,
-        .receive_handler = &rc522_receive,
+        .driver = driver,
     };
 
-    rc522_create(&config, &rc522_handle);
-    rc522_register_events(rc522_handle, RC522_EVENT_ANY, rc522_event_handler, NULL);
-    rc522_start(rc522_handle);
+    rc522_create(&config, &rc522);
+    rc522_register_events(rc522, RC522_EVENT_ANY, rc522_event_handler, NULL);
+    rc522_start(rc522);
 }
