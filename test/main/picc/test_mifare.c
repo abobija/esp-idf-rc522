f82c59b76dcc548c62e4b1f6e1f1d8d416b8d2f4
Alija Bobija <bobija.alija@gmail.com>
2024-09-18 13:57:14 +0200

    unit testing mifare number of sectors

--- test/main/picc/test_mifare.c
+++ test/main/picc/test_mifare.c
@@ -2,6 +2,33 @@
 
 #include "picc/rc522_mifare.c"
 
+TEST_CASE("test_MIFAREMini_has_5_sectors", "[mifare]")
+{
+    uint8_t result;
+    TEST_ASSERT_EQUAL(ESP_OK, rc522_mifare_number_of_sectors(RC522_PICC_TYPE_MIFARE_MINI, &result));
+    TEST_ASSERT_EQUAL_UINT8(5, result);
+}
+
+TEST_CASE("test_MIFARE1K_has_16_sectors", "[mifare]")
+{
+    uint8_t result;
+    TEST_ASSERT_EQUAL(ESP_OK, rc522_mifare_number_of_sectors(RC522_PICC_TYPE_MIFARE_1K, &result));
+    TEST_ASSERT_EQUAL_UINT8(16, result);
+}
+
+TEST_CASE("test_MIFARE4K_has_40_sectors", "[mifare]")
+{
+    uint8_t result;
+    TEST_ASSERT_EQUAL(ESP_OK, rc522_mifare_number_of_sectors(RC522_PICC_TYPE_MIFARE_4K, &result));
+    TEST_ASSERT_EQUAL_UINT8(40, result);
+}
+
+TEST_CASE("test_Error_sector_numbers_for_no_MIFARE_picc", "[mifare]")
+{
+    uint8_t result;
+    TEST_ASSERT_NOT_EQUAL(ESP_OK, rc522_mifare_number_of_sectors(RC522_PICC_TYPE_UNKNOWN, &result));
+}
+
 TEST_CASE("test_MIFARE1K_is_classic_compatible", "[mifare]")
 {
     TEST_ASSERT_TRUE(rc522_mifare_type_is_classic_compatible(RC522_PICC_TYPE_MIFARE_1K));
