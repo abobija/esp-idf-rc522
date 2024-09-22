ee2e607d807f968dcf78e02da391967b63fa2829
Alija Bobija <bobija.alija@gmail.com>
2024-09-22 15:34:00 +0200

    Use term 'reader' in other examples as well

--- examples/basic/main/basic.c
+++ examples/basic/main/basic.c
@@ -8,8 +8,8 @@ static const char *TAG = "rc522-basic-example";
 #define RC522_SPI_BUS_GPIO_MISO   (25)
 #define RC522_SPI_BUS_GPIO_MOSI   (23)
 #define RC522_SPI_BUS_GPIO_SCLK   (19)
-#define RC522_SPI_DEVICE_GPIO_SDA (22)
-#define RC522_GPIO_RST            (-1) // soft-reset
+#define RC522_SPI_READER_GPIO_SDA (22)
+#define RC522_READER_GPIO_RST     (-1) // soft-reset
 
 static rc522_spi_config_t driver_config = {
     .host_id = VSPI_HOST,
@@ -19,13 +19,13 @@ static rc522_spi_config_t driver_config = {
         .sclk_io_num = RC522_SPI_BUS_GPIO_SCLK,
     },
     .dev_config = {
-        .spics_io_num = RC522_SPI_DEVICE_GPIO_SDA,
+        .spics_io_num = RC522_SPI_READER_GPIO_SDA,
     },
-    .rst_io_num = RC522_GPIO_RST,
+    .rst_io_num = RC522_READER_GPIO_RST,
 };
 
 static rc522_driver_handle_t driver;
-static rc522_handle_t rc522;
+static rc522_handle_t reader;
 
 static void on_picc_state_changed(void *arg, esp_event_base_t base, int32_t event_id, void *data)
 {
@@ -45,11 +45,11 @@ void app_main()
     rc522_spi_create(&driver_config, &driver);
     rc522_driver_install(driver);
 
-    rc522_config_t config = {
+    rc522_config_t reader_config = {
         .driver = driver,
     };
 
-    rc522_create(&config, &rc522);
-    rc522_register_events(rc522, RC522_EVENT_PICC_STATE_CHANGED, on_picc_state_changed, NULL);
-    rc522_start(rc522);
+    rc522_create(&reader_config, &reader);
+    rc522_register_events(reader, RC522_EVENT_PICC_STATE_CHANGED, on_picc_state_changed, NULL);
+    rc522_start(reader);
 }
