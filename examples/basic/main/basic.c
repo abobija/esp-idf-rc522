32a45af8d934934d457aff02023f3eba096ed74e
Alija Bobija <bobija.alija@gmail.com>
2024-09-22 07:19:12 +0200

    Overwriting SPI config with defaults

--- examples/basic/main/basic.c
+++ examples/basic/main/basic.c
@@ -13,20 +13,13 @@ static const char *TAG = "rc522-basic-example";
 
 static rc522_spi_config_t driver_config = {
     .host_id = VSPI_HOST,
-    .dma_chan = SPI_DMA_DISABLED,
     .bus_config = &(spi_bus_config_t){
         .miso_io_num = RC522_SPI_BUS_GPIO_MISO,
         .mosi_io_num = RC522_SPI_BUS_GPIO_MOSI,
         .sclk_io_num = RC522_SPI_BUS_GPIO_SCLK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
     },
     .dev_config = {
-        .clock_speed_hz = 5000000,
-        .mode = 0,
         .spics_io_num = RC522_SPI_DEVICE_GPIO_SDA,
-        .queue_size = 7,
-        .flags = SPI_DEVICE_HALFDUPLEX,
     },
     .rst_io_num = RC522_GPIO_RST,
 };
