fb0233b3d404e84fdfeeeb450f9c18113b9a7548
Piotr Koronczok <108998301+triplo098@users.noreply.github.com>
2026-07-24 16:27:57 +0200

    Add gain configuration support (#105)
    Added gain configuration support based on issue #99, the receiver gain
    can be setup after `rc522_start(scanner)` by calling
    `rc522_pcd_set_rx_gain(scanner, RC522_PCD_48_DB_RX_GAIN)`.
    
    ---------
    
    Co-authored-by: Alija Bobija <bobija.alija@gmail.com>

--- internal/rc522_pcd_internal.h
+++ internal/rc522_pcd_internal.h
@@ -233,26 +233,6 @@ enum // RC522_PCD_TX_CONTROL_REG
     RC522_PCD_TX1_RF_EN_BIT = BIT0,
 };
 
-enum // RC522_PCD_RF_CFG_REG
-{
-    RC522_PCD_RX_GAIN_2_BIT = BIT6,
-    RC522_PCD_RX_GAIN_1_BIT = BIT5,
-    RC522_PCD_RX_GAIN_0_BIT = BIT4,
-};
-
-/**
- * Receiver (antenna) gain
- */
-typedef enum
-{
-    RC522_PCD_18_DB_RX_GAIN = (RC522_PCD_RX_GAIN_1_BIT),                                                     /* 18 dB */
-    RC522_PCD_23_DB_RX_GAIN = (RC522_PCD_RX_GAIN_1_BIT | RC522_PCD_RX_GAIN_0_BIT),                           /* 23 dB */
-    RC522_PCD_33_DB_RX_GAIN = (RC522_PCD_RX_GAIN_2_BIT),                                                     /* 33 dB */
-    RC522_PCD_38_DB_RX_GAIN = (RC522_PCD_RX_GAIN_2_BIT | RC522_PCD_RX_GAIN_0_BIT),                           /* 38 dB */
-    RC522_PCD_43_DB_RX_GAIN = (RC522_PCD_RX_GAIN_2_BIT | RC522_PCD_RX_GAIN_1_BIT),                           /* 43 dB */
-    RC522_PCD_48_DB_RX_GAIN = (RC522_PCD_RX_GAIN_2_BIT | RC522_PCD_RX_GAIN_1_BIT | RC522_PCD_RX_GAIN_0_BIT), /* 48 dB */
-} rc522_pcd_rx_gain_t;
-
 typedef enum
 {
     /**
